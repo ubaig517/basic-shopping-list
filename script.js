b3609a9befbf54f12f301564457b692be8a5d51c
Umair <ubaig517@gmail.com>
2018-09-27 11:03:09 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,43 @@
+var userInput = document.querySelector("#user-input");
+var button = document.querySelector("#button");
+var ul = document.querySelector("#list");
+
+function inputLength() {
+    return userInput.value.length
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  var deleteButton = document.createElement("button");
+  li.appendChild(document.createTextNode(userInput.value));
+  li.appendChild(deleteButton);
+  deleteButton.innerHTML = "Delete";
+  deleteButton.classList.add("delete-button");
+  ul.appendChild(li);
+  userInput.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0){
+    createListElement();
+  }
+}
+
+function addListAfterEnter(event) {
+  if (inputLength() > 0 && event.keyCode === 13){
+    createListElement();
+  }
+}
+
+ul.addEventListener("click", function(e) {
+  if(e.target.nodeName == "LI"){
+    e.target.classList.toggle("done");
+  } else if(e.target.nodeName == "BUTTON") {
+    e.target.parentNode.remove();
+  }
+});
+
+
+button.addEventListener("click", addListAfterClick);
+
+userInput.addEventListener("keypress", addListAfterEnter);
